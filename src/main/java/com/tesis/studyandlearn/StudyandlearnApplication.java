f4626e3a395bd311d3c5d0c75aa8b8da27cf76b1
nvenegas96 <55160513+nvenegas96@users.noreply.github.com>
2020-11-10 13:38:48 -0300

    Mi Primer Commit

---
+++ src/main/java/com/tesis/studyandlearn/StudyandlearnApplication.java
@@ -0,0 +1,13 @@
+package com.tesis.studyandlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyandlearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyandlearnApplication.class, args);
+    }
+
+}
